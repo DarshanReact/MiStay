129e0c5c9b27735c5c42a6a057ae64b8aa682ce6
DarshanReact <59929206+DarshanReact@users.noreply.github.com>
2020-01-15 22:51:12 +0530

    build commit resolved

---
+++ build/precache-manifest.6d444877951465fdcda0d8caf80e1df7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c990ca094f289b07ea29bca80dd2bdb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d695edba5b699ca6866a",
+    "url": "/static/css/main.9e3044f4.chunk.css"
+  },
+  {
+    "revision": "0a40c8fefa0bbfb26af8",
+    "url": "/static/js/2.22370a05.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.22370a05.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d695edba5b699ca6866a",
+    "url": "/static/js/main.3d9b499d.chunk.js"
+  },
+  {
+    "revision": "b75edf7407ee76cfc28b",
+    "url": "/static/js/runtime-main.059a53fb.js"
+  }
+]);
